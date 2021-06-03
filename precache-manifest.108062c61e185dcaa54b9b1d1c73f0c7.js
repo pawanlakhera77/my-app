6ab87cac37304c8bbb828fd70d9b094f894f802e
Pawan lakhera <pawanlakhera77@gmail.com>
2021-06-03 18:56:18 +0530

    Updates

---
+++ precache-manifest.108062c61e185dcaa54b9b1d1c73f0c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2249f746f73d464cf244b21ed424373b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ad92bfd3c69a587d7eae",
+    "url": "/my-app/static/css/main.837db68a.chunk.css"
+  },
+  {
+    "revision": "a5d0ff6c478aa5b433aa",
+    "url": "/my-app/static/js/2.755c874a.chunk.js"
+  },
+  {
+    "revision": "eadc8ac8b6080df510389a008a76ce0a",
+    "url": "/my-app/static/js/2.755c874a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad92bfd3c69a587d7eae",
+    "url": "/my-app/static/js/main.b50275c3.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/my-app/static/media/logo.ee7cd8ed.svg"
+  }
+]);
